a2bfe51b36d99b6fea42020b6ee1c2766083ddb7
nikolay.rogovoy@gmail.com <nikolay.rogovoy@gmail.com>
2017-11-20 15:52:38 +0300

    Добавлена подсветка ячеек

--- lib/at-grid.ts
+++ lib/at-grid.ts
@@ -13,146 +13,154 @@ import {NgForm} from '@angular/forms';
 })
 export class AtGrid implements  OnInit, AfterViewChecked {
 
-  /**Данные*/
-  @Input()
-  data: Array<Object> = [];
+    /**Данные*/
+    @Input()
+    data: Array<Object> = [];
 
-  /**Колонки*/
-  @Input()
-  metaData: ColumnInfo[] = [];
+    /**Колонки*/
+    @Input()
+    metaData: ColumnInfo[] = [];
 
-  /**Количество строк на странице*/
-  @Input()
-  quRowOnPage: number = 10;
+    /**Количество строк на странице*/
+    @Input()
+    quRowOnPage: number = 10;
 
-  /**Статический режим*/
-  @Input()
-  staticMode: boolean = true;
+    /**Статический режим*/
+    @Input()
+    staticMode: boolean = true;
 
-  /**Текущая страница*/
-  @Input()
-  currentPage: number = 0;
+    /**Текущая страница*/
+    @Input()
+    currentPage: number = 0;
 
-  /**Текущая страница*/
-  @Input()
-  imageSize: number = 100;
+    /**Текущая страница*/
+    @Input()
+    imageSize: number = 100;
 
-  /**Выделене какой-то позиции*/
-  @Output()
-  onSelect = new EventEmitter<Object>();
+    /**Для установки стиля ячейки (row: Object, column: ColumnInfo) => {({'background-color': 'red'})}*/
+    @Input()
+    callbackSetCellStyle: Function;
 
-  /**Форма*/
-  @ViewChild('myForm')
-  form: NgForm;
+    /**Выделене какой-то позиции*/
+    @Output()
+    onSelect = new EventEmitter<Object>();
 
-  /**Номер строки с наведенной мышкой*/
-  rowNumMouseOver: number;
+    /**Форма*/
+    @ViewChild('myForm')
+    form: NgForm;
 
-  /**Выделене какой-то позиции*/
-  @Output()
-  onLoad = new EventEmitter<number>();
+    /**Номер строки с наведенной мышкой*/
+    rowNumMouseOver: number;
 
-  /**Энум в компонет*/
-  public columnFormat = ColumnFormat;
+    /**Выделене какой-то позиции*/
+    @Output()
+    onLoad = new EventEmitter<number>();
 
-  /**Инит компонента*/
-  ngOnInit() {
-  }
+    /**Энум в компонет*/
+    public columnFormat = ColumnFormat;
 
-  /**После загрузки вьюхи*/
-  ngAfterViewChecked() {
-  }
-
-  /**Выделить строку*/
-  selectRow(item: Object) {
-    this.onSelect.emit(item);
-  }
-
-  /**Первая страница*/
-  firstPage() {
-    this.currentPage = 0;
-  }
-
-  /**Первая страница*/
-  lastPage() {
-    this.currentPage = this.getQuPage() - 1;
-  }
-
-  /**Первая страница*/
-  nextPage() {
-    if (this.currentPage < (this.getQuPage() - 1)) {
-      this.currentPage++;
-    } else {
-      this.currentPage = this.getQuPage() - 1;
+    constructor() {
+        this.callbackSetCellStyle = (row: Object, column: ColumnInfo) => {};
     }
-  }
 
-  /**Первая страница*/
-  prevPage() {
-    if (this.currentPage > 0) {
-      this.currentPage--;
-    } else {
-      this.currentPage = 0;
+    /**Инит компонента*/
+    ngOnInit() {
     }
-  }
 
-  /**Количество страниц*/
-  getQuPage(): number {
-    let result = Math.ceil(this.applyFilter().length / this.quRowOnPage);
-    if (result <= 0) {
-      result = 1;
+    /**После загрузки вьюхи*/
+    ngAfterViewChecked() {
     }
-    return result;
-  }
 
-  /**Номер страницы от 1*/
-  getHumanCurrentPage(): number {
-    return this.currentPage + 1;
-  }
+    /**Выделить строку*/
+    selectRow(item: Object) {
+        this.onSelect.emit(item);
+    }
 
-  applyFilter(): Array<Object> {
-    let result: Array<Object> = [];
+    /**Первая страница*/
+    firstPage() {
+        this.currentPage = 0;
+    }
 
-    for (let item of this.data) {
-      let checkFilter = true;
-      for (let column of this.metaData) {
-        if (column.filterInfo.value !== '') {
-          if (item[column.name] != null && item[column.name].toString().indexOf(column.filterInfo.value) !== -1) {
-            // Условие правильное
-          } else {
-            checkFilter = false;
-            break;
-          }
+    /**Первая страница*/
+    lastPage() {
+        this.currentPage = this.getQuPage() - 1;
+    }
+
+    /**Первая страница*/
+    nextPage() {
+        if (this.currentPage < (this.getQuPage() - 1)) {
+            this.currentPage++;
+        } else {
+            this.currentPage = this.getQuPage() - 1;
         }
-      }
-
-      if (checkFilter) {
-        result.push(item);
-      }
     }
-    return result;
-  }
 
-  /**Получить строки для страницы*/
-  getRowForPage(): Array<Object> {
-    let index = 0;
-    let result: Array<Object> = [];
-    for (let item of this.applyFilter()) {
-      if (index >= this.currentPage * this.quRowOnPage && index < (this.currentPage + 1) * this.quRowOnPage) {
-        result.push(item);
-      }
-      index++;
+    /**Первая страница*/
+    prevPage() {
+        if (this.currentPage > 0) {
+            this.currentPage--;
+        } else {
+            this.currentPage = 0;
+        }
     }
-    return result;
-  }
 
-  /**Фильтр изменен*/
-  filterChanged(filter: any) {
-    console.log(filter);
-  }
+    /**Количество страниц*/
+    getQuPage(): number {
+        let result = Math.ceil(this.applyFilter().length / this.quRowOnPage);
+        if (result <= 0) {
+            result = 1;
+        }
+        return result;
+    }
 
-  mouseOver(rowNum: number) {
-    this.rowNumMouseOver = rowNum;
-  }
+    /**Номер страницы от 1*/
+    getHumanCurrentPage(): number {
+        return this.currentPage + 1;
+    }
+
+    applyFilter(): Array<Object> {
+        let result: Array<Object> = [];
+
+        for (let item of this.data) {
+            let checkFilter = true;
+            for (let column of this.metaData) {
+                if (column.filterInfo.value !== '') {
+                    if (item[column.name] != null && item[column.name].toString().indexOf(column.filterInfo.value) !== -1) {
+                        // Условие правильное
+                    } else {
+                        checkFilter = false;
+                        break;
+                    }
+                }
+            }
+
+            if (checkFilter) {
+                result.push(item);
+            }
+        }
+        return result;
+    }
+
+    /**Получить строки для страницы*/
+    getRowForPage(): Array<Object> {
+        let index = 0;
+        let result: Array<Object> = [];
+        for (let item of this.applyFilter()) {
+            if (index >= this.currentPage * this.quRowOnPage && index < (this.currentPage + 1) * this.quRowOnPage) {
+                result.push(item);
+            }
+            index++;
+        }
+        return result;
+    }
+
+    /**Фильтр изменен*/
+    filterChanged(filter: any) {
+        console.log(filter);
+    }
+
+    mouseOver(rowNum: number) {
+        this.rowNumMouseOver = rowNum;
+    }
 
 }
